003d958ca1ce6218f6555ffd59971fe64bb3dbfa
Elize Marie Gilberte Leto <elizemariegilberteleto@macbook-pro-2.lan>
2026-06-16 11:47:51 +0200

    Fix dotenv production

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const https = require('https');
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 const app = express();
 app.use(cors());
